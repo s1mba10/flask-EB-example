e13c27f44f310c1b5946408a3fd26882e9d4fbcc
Danil Golubintsev <danilmpower@gmail.com>
2023-03-10 13:14:23 +0300

    init

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return '<h1>Hello, welcome to my main page!</h1>'
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True, port=5000)
+    app.run(host='127.0.0.1', debug=True, port=80)
