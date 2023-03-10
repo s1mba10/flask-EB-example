951342d2e075d8524bdcaea35ffe7198c1ba9b83
Danil Golubintsev <danilmpower@gmail.com>
2023-03-10 16:25:24 +0300

    fixing bugs

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     return '<h1>Hello, welcome to my main page!</h1>'
