a3b1c37ef70e7656a832346df6b55613d41fdbdc
Danil Golubintsev <danilmpower@gmail.com>
2023-03-10 16:22:01 +0300

    init

--- app.py
+++ app.py
@@ -5,6 +5,3 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello, welcome to my main page!</h1>'
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True, port=80)
